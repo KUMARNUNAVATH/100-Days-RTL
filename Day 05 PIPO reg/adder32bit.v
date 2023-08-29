925818a73a717a12982209aadfa7fcf4856157ff
Nunavath Kumar <101395036+KUMARNUNAVATH@users.noreply.github.com>
2023-08-29 15:46:23 +0530

    Update adder32bit.v

--- Day 05 PIPO reg/adder32bit.v
+++ Day 05 PIPO reg/adder32bit.v
@@ -1,5 +1,5 @@
 
-/*module PIPO1(I,sel,clk,QB);
+module PIPO1(I,sel,clk,QB);
 input  [31:0]I;
 input sel,clk;
 output reg [31:0]QB;
@@ -9,12 +9,5 @@ assign y= (sel)?I:32'b0;
 always @(posedge clk) 
 QB<=y;
 endmodule 
-*/
-module PIPO2(clk,QA,D);
-input clk;
-input [32:0] D;
-output reg[32:0]QA;
-always@(posedge clk)
-QA<=D;
 
-endmodule
+
